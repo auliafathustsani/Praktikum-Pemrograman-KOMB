9a59f1802553f9a55aa2d915bc67c23f0a706010
auliafathustsani <auliafathustsani@mail.ugm.ac.id>
2024-09-17 14:58:46 +0700

    Update multiplication.cpp

--- Pertemuan 4/soal 2/multiplication.cpp
+++ Pertemuan 4/soal 2/multiplication.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	cout<<"Input :";
+	cin>>n;
+	
+	cout<<"Output :"<<endl;
+	for (int i { 1 }; i <= 10; ++i ){
+ 		cout << n << " * " << i << " = " << n * i<<endl;
+	}
+
+	return 0;
+}
 
