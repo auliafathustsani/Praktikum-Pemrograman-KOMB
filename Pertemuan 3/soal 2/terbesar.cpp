a413b98c5938c4b396133bfe4004f00eb187b95b
auliafathustsani <auliafathustsani@mail.ugm.ac.id>
2024-09-17 16:04:00 +0700

    Update terbesar.cpp

--- Pertemuan 3/soal 2/terbesar.cpp
+++ Pertemuan 3/soal 2/terbesar.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a, b, c;
 
+    cout << "Inputkan bilangan 1 : ";
+    cin >> a;
+    cout << "Inputkan bilangan 2 : ";
+    cin >> b;
+    cout << "Inputkan bilangan 3 : ";
+    cin >> c;
+    
+    switch (a > b) {
+      case 1 :
+       switch(a > c){
+            case 1 :
+               cout << "Bilangan terbesar adalah = " << a;
+               break;
+            case 0 :
+               cout << "Bilangan terbesar adalah = " << c;
+               break;
+        }
+        break;
+      case 0 :
+        switch(b > c){
+            case 1 :
+               cout << "Bilangan terbesar adalah = " << b;
+               break;
+            case 0 :
+               cout << "Bilangan terbesar adalah = " << c;
+               break;
+        }
+        break;
+    }
+    return 0;
+}
