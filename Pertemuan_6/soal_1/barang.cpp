3325b5c680fc4f1e699042018471de01a95db104
auliafathustsani <auliafathustsani@mail.ugm.ac.id>
2024-10-19 10:08:56 +0700

    Update barang.cpp

--- Pertemuan_6/soal_1/barang.cpp
+++ Pertemuan_6/soal_1/barang.cpp
@@ -12,12 +12,12 @@ struct barang{
 int main() {
     int n=3; 
     int totaljumlah=0;
-	  float totalharga=0.0, ratarataharga;	
+	float totalharga=0.0, ratarataharga;	
 	
     barang barang [50]={
         {"101", "Beras", 15000.0, 5},
-	      {"201", "Minyak", 14000.0, 4},
-	      {"301", "Garam", 5000.0, 7}
+	{"201", "Minyak", 14000.0, 4},
+	{"301", "Garam", 5000.0, 7}
     };
     
     for (int i=0; i < n; i++){
