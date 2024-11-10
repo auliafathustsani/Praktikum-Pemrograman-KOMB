2c7a5f529e9877d806588b96822eaf2362ee3565
auliafathustsani <auliafathustsani@mail.ugm.ac.id>
2024-11-10 18:45:16 +0700

    Update fibonacci.cpp

--- Pertemuan_8/soal_1/fibonacci.cpp
+++ Pertemuan_8/soal_1/fibonacci.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int fibonacci(int n) {
+    if (n==0 || n==1) {
+        return n;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+int main() {
+    int n = 5;
+    int expected_output = fibonacci(n);
+    cout << "Fibonacci ke-" << n << " adalah " << expected_output;
+
+	if (expected_output == 5){
+		cout << "Test Passed" << endl;
+    } else {
+        cout << "Test Failed" << endl;
+	}
+	
+	return 0;
+}
